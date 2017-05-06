e72c9631aa6a354791d8646e8941d02cb1b9e22e
Kaan Ozkan <kaanozkan97@gmail.com>
2017-05-05 23:52:45 -0400

    Initial Commit

---
+++ Shopify Merchant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopify Merchant
+//
+//  Created by Kaan Ozkan on 2017-05-05.
+//  Copyright © 2017 Kaan Ozkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
